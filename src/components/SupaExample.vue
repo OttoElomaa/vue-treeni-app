3d4d948881d2d0d5acfe5232a8abbfbdf6ac11fd
Otto Elomaa <otto.elomaa99@gmail.com>
2026-02-24 23:33:17 +0200

    first commit. vue hello world with supabase client

---
+++ src/components/SupaExample.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { supabase } from '../lib/supabase-client'
+
+
+const testvalues = ref<any[] | null>([])
+async function getTest() {
+  const { data } = await supabase.from('test_table').select()
+  testvalues.value = data
+}
+onMounted(() => {
+  getTest()
+})
+</script>
+
+
+<template>
+  <h1>Showing some test data from supabase:</h1>
+  <ul>
+    <li v-for="t in testvalues" :key="t.id">{{ t.test_idk }}</li>
+  </ul>
+</template>
