53d08cde24559ace1506eaa12dc90e3603f45bc7
Otto Elomaa <otto.elomaa99@gmail.com>
2026-03-23 18:55:30 +0200

    AddPlayer: First steps setting up PrimeVue Form input. Has first name field for trying it out, doesn't send anything yet

--- src/components/intake/AddPlayer.vue
+++ src/components/intake/AddPlayer.vue
@@ -1,7 +1,43 @@
 <script setup lang="ts">
+import { Form } from '@primevue/forms';
+import { zodResolver } from '@primevue/forms/resolvers/zod';
+import { PlayerSchema } from '../../types';
+import { reactive, ref } from 'vue';
+import { Button, InputText, Message } from 'primevue';
+import MyIntakeErrorMessage from './MyIntakeErrorMessage.vue';
+
+
+const resolver = zodResolver(PlayerSchema);
+
+const initialValues = reactive({
+	id: null
+});
+
+const testTestTest = ref(false)
+
+const onFormSubmit = () => {
+	testTestTest.value = true
+}
 
 </script>
 
 <template>
-	<h1 class="text-2xl">Intake Screen - Add Player</h1>
+	<div class="grid gap-4 grid-cols-1">
+		<h1 class="text-2xl">Intake Screen - Add Player</h1>
+		<p v-if="testTestTest">Submitted!</p>
+
+
+
+
+		<Form v-slot="$form" :initialValues :resolver="resolver" @submit="onFormSubmit" :validateOnValueUpdate="true"
+			class="flex flex-col gap-4">
+			<div class="flex flex-col gap-1">
+				<InputText name="first_name" type="text" placeholder="Etunimi" fluid />
+				<MyIntakeErrorMessage :inputValue="$form.first_name" />
+			</div>
+			<Button type="submit" severity="secondary" label="Submit" />
+		</Form>
+	</div>
+
+
 </template>
---
+++ src/components/intake/MyIntakeErrorMessage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Message } from 'primevue';
+
+
+const props = defineProps(['inputValue'])
+
+</script>
+
+
+<template>
+	<Message v-if="inputValue?.invalid" severity="error" size="small" variant="simple">
+		{{ inputValue.error.message }}
+	</Message>
+
+</template>
