1c43e9ee75eea321b1302f1f24e3d4180d188b33
Otto Elomaa <otto.elomaa99@gmail.com>
2026-03-23 19:15:30 +0200

    AddPlayer: Added all fields and some styling

--- src/components/intake/AddPlayer.vue
+++ src/components/intake/AddPlayer.vue
@@ -3,8 +3,13 @@ import { Form } from '@primevue/forms';
 import { zodResolver } from '@primevue/forms/resolvers/zod';
 import { PlayerSchema } from '../../types';
 import { reactive, ref } from 'vue';
-import { Button, InputText, Message } from 'primevue';
+import { Button, InputNumber, InputText, Message } from 'primevue';
 import MyIntakeErrorMessage from './MyIntakeErrorMessage.vue';
+
+
+const props = defineProps<{
+	teamId: number
+}>()
 
 
 const resolver = zodResolver(PlayerSchema);
@@ -14,9 +19,10 @@ const initialValues = reactive({
 });
 
 const testTestTest = ref(false)
+const isInvalidSubmit = ref(false)
 
 const onFormSubmit = () => {
-	testTestTest.value = true
+	isInvalidSubmit.value = true
 }
 
 </script>
@@ -31,10 +37,33 @@ const onFormSubmit = () => {
 
 		<Form v-slot="$form" :initialValues :resolver="resolver" @submit="onFormSubmit" :validateOnValueUpdate="true"
 			class="flex flex-col gap-4">
-			<div class="flex flex-col gap-1">
-				<InputText name="first_name" type="text" placeholder="Etunimi" fluid />
-				<MyIntakeErrorMessage :inputValue="$form.first_name" />
+			<!-- First Name and Last name -->
+			<div class="flex flex-row gap-4">
+				<div class="flex flex-col gap-1">
+					<InputText name="first_name" type="text" placeholder="Etunimi" fluid />
+					<MyIntakeErrorMessage :inputValue="$form.first_name" />
+				</div>
+				<div class="flex flex-col gap-1">
+					<InputText name="last_name" type="text" placeholder="Sukunimi" fluid />
+					<MyIntakeErrorMessage :inputValue="$form.last_name" />
+				</div>
 			</div>
+			<!-- Birth Year, Month, Day -->
+			<div class="flex flex-row gap-4">
+				<div class="flex flex-col gap-1">
+					<InputNumber name="birth_day" placeholder="Syntymävuosi" :useGrouping="false" fluid />
+					<MyIntakeErrorMessage :inputValue="$form.birth_day" />
+				</div>
+				<div class="flex flex-col gap-1">
+					<InputNumber name="birth_month" placeholder="Syntymävuosi" :useGrouping="false" fluid />
+					<MyIntakeErrorMessage :inputValue="$form.birth_month" />
+				</div>
+				<div class="flex flex-col gap-1">
+					<InputNumber name="birth_year" placeholder="Syntymävuosi" :useGrouping="false" fluid />
+					<MyIntakeErrorMessage :inputValue="$form.birth_year" />
+				</div>
+			</div>
+			<p v-if="isInvalidSubmit">Annetuissa tiedoissa on jokin ongelma, yritä uudestaan!</p>
 			<Button type="submit" severity="secondary" label="Submit" />
 		</Form>
 	</div>
